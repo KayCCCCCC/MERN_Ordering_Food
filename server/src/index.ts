c57fa1159a27c25800875019ff57873aa9911353
NguyenNha <nguyenngothanhnha2003kg@gmail.com>
2024-08-19 23:31:47 +0700

    change port

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,6 +18,6 @@ app.get("/", (req: Request, res: Response) => {
 
 app.use("/api", router)
 
-app.listen(8000, () => {
-    console.log("server started on http://localhost:8000")
+app.listen(7000, () => {
+    console.log("server started on http://localhost:7000")
 })

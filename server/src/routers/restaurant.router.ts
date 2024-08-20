70a871e1359598498a7f2275d383f9269c4379cd
NguyenNha <nguyenngothanhnha2003kg@gmail.com>
2024-08-20 21:18:57 +0700

    search filter

--- server/src/routers/restaurant.router.ts
+++ server/src/routers/restaurant.router.ts
@@ -3,6 +3,7 @@ import multer from "multer";
 import restaurantController from "../controllers/restaurant.controller";
 import { jwtCheck, jwtParse } from "../middlewares/auth";
 import { validateMyRestaurantRequest } from "../middlewares/validation";
+import { param } from "express-validator";
 
 const router = express.Router();
 const storage = multer.memoryStorage();
@@ -18,5 +19,9 @@ router.patch("/my-order/:orderId/status", jwtCheck, jwtParse, restaurantControll
 router.get("/my-get-info", jwtCheck, jwtParse, restaurantController.getMyRestaurant);
 router.post("/my-create", upload.single("imageFile"), validateMyRestaurantRequest, jwtCheck, jwtParse, restaurantController.createRestaurant)
 router.put("/my-update", upload.single("imageFile"), validateMyRestaurantRequest, jwtCheck, jwtParse, restaurantController.updateMyRestaurant);
+
+// Restaurant
+router.get("/get/:restaurantId", param("restaurantId").isString().trim().notEmpty().withMessage("RestaurantId paramenter must be a valid string"), restaurantController.getRestaurant);
+router.get("/search/:city", param("city").isString().trim().notEmpty().withMessage("City paramenter must be a valid string"), restaurantController.searchRestaurant);
 
 export default router;
